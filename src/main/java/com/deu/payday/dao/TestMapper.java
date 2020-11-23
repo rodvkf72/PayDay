16484c5314ca4b56290ac64ff844a5bc81edbdbf
rodvkf72 <48707324+rodvkf72@users.noreply.github.com>
2020-11-23 16:38:21 +0900

    Add: Project File
    프로젝트 파일

---
+++ src/main/java/com/deu/payday/dao/TestMapper.java
@@ -0,0 +1,12 @@
+package com.deu.payday.dao;
+
+//import java.util.HashMap;
+import java.util.List;
+
+import com.deu.payday.util.PubMap;
+
+public interface TestMapper {
+
+    List<PubMap> selectTestData();
+
+}
